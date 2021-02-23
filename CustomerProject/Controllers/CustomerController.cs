35e17ea3ad568ef6d0dee1ac9e1a396e11bb64c8
anna.mamikonyan <anna.mamikonyan@technamin.com>
2021-02-23 13:11:31 +0400

    add image

--- CustomerProject/Controllers/CustomerController.cs
+++ CustomerProject/Controllers/CustomerController.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace CustomerProject.Controllers
 {
     public class CustomerController : Controller
     {
         public async Task<ViewResult> Customers()
-
         {
             //var result1 = Data.Customers.CustomerWIthSqlDataAdapter.GetCustomersFromDBAsync1();
             var result = new List<CustomerProject.Models.Customer>();
@@ -34,12 +38,24 @@ namespace CustomerProject.Controllers
             return View();
         }
 
-
         [HttpPost]
-        public  async Task<ViewResult> Customers(Models.Customer newCustomer)
+        public  async Task<ViewResult> Customers(Models.Customer newCustomer, HttpPostedFileBase avatar)
         {
-            Data.Customers.Customer cust = new Data.Customers.Customer();
+            Data.Customers.Customer cust = new Data.Customers.Customer() 
+            { };
+
+
+
+            String folderPath = HostingEnvironment.MapPath("~/App_Data/Images") ;
+            Directory.CreateDirectory(folderPath);
+
+            string avatarPath = Path.Combine(folderPath, avatar.FileName);
             Data.Customers.CutomerLibrary.AddCustomer(cust);
+
+            if (!System.IO.File.Exists(avatarPath))
+            {
+               avatar.SaveAs(avatarPath);
+            }
 
             var result = new List<CustomerProject.Models.Customer>();
             var customerList = await Data.Customers.CutomerLibrary.GetCustomersFromDBAsync();
